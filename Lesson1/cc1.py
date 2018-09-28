9f78c2a920969b8c757db00a7263e89a53dd26fc
Theo Nazon <theonazon@gmail.com>
2018-09-28 14:44:10 +0200

    Modification sur text

--- Lesson1/cc1.py
+++ Lesson1/cc1.py
@@ -34,7 +34,7 @@ def last2(string):
 # text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 def occurences(text):
     result_dict = {}
-    for i in text:
+    for i in text.lower():
         if i in result_dict.keys():
             result_dict[i] += 1
         else:
