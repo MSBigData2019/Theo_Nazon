f85c2e3bdc4f37a1e3d67fa9fb687e8c82389e52
Theo Nazon <theonazon@gmail.com>
2018-10-19 16:06:58 +0200

    Exo 4 finalise perso

--- INFMDI721/Lesson4/exo_cc_lesson_4.py
+++ INFMDI721/Lesson4/exo_cc_lesson_4.py
@@ -1,26 +1,19 @@
-# Accept: application/vnd.github.v3+json
 import requests
-import unittest
-from bs4 import BeautifulSoup
-import re
 import json
 import pandas as pd
-import time
 
+df = pd.read_excel("/home/theo/Dropbox/list_cities.xls", headers=None)[:10]
+df["Ville"] = df["Ville"] + ", France"
 
-df = pd.read_excel("/home/theo/Dropbox/list_cities.xls", headers=None)[:3]
 list_of_cities_for_API = "|".join(df["Ville"])
 api_key = pd.read_csv("/home/theo/Documents/MASTER/Theo_Nazon/INFMDI721/Lesson4/api_key.txt", header = None).iloc[0][0]
 
 
 def get_distance_between_two_cities(list_of_cities_for_API):
-
     url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins={}&destinations={}&key={}".format(list_of_cities_for_API, list_of_cities_for_API, api_key)
     res = requests.get(url)
     response_object = json.loads(res.text)
-
     distances = list(map(lambda x : list(map(lambda y : y['distance']['text'], x['elements'])), response_object['rows']))
-
     distance_matrix = pd.DataFrame(distances, columns=df["Ville"], index=df["Ville"])
     return distance_matrix
 
